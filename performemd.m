f66f7ac72dab76de0e24dc926ad31500371e6cd2
Ana Matoso <anamatoso92@gmail.com>
2021-07-25 15:56:06 +0100

    Update performemd.m

--- performemd.m
+++ performemd.m
@@ -1,4 +1,13 @@
 function [M] = performemd(Mpatients)
+% This function performs EMD to a column of patients and puts each IMFs of
+% each patient in the respective row, according to the patient, and column
+% according to the signal.
+%
+% Input:    Mpatients - Column cell in which each row is a patient
+%
+% Output:   M - Matrix cell in which each row is a patient. In the first
+% column is the original signal, in the second one, the EMD of the first
+% signal, in the third one, the EMD of the second, and so on.
 
 M=Mpatients;
 n_patients=length(M);
